a353cbc8f339c8c2ee37bfe3c73751953780bbef
amit <asj8139@g.rit.edu>
2018-01-20 19:31:33 -0500

    Added documentation.

--- src/main/java/utils/ApplicationProperties.java
+++ src/main/java/utils/ApplicationProperties.java
@@ -5,10 +5,25 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
+/**
+ * The {@code {@link ApplicationProperties}} provides a simple way to load all properties required by
+ * a java project.
+ *
+ * @author     : Amit Jaisinghani
+ *
+ */
 public class ApplicationProperties {
 
+    //Holds loaded properties
     private static Properties properties = new Properties();
 
+    /**
+     * Load properties from an InputStream and provides an option to restriction on overriding property.
+     *
+     * @param file InputStream of file containing properties to load
+     * @param override Restriction on overriding property
+     * @throws Exception if override is false and duplicate property is found, an exception is raised.
+     */
     public static void loadProperties(InputStream file, boolean override) throws Exception {
         Properties temporaryProperties = new Properties();
         temporaryProperties.load(file);
@@ -26,16 +41,35 @@ public class ApplicationProperties {
         properties.putAll(temporaryProperties);
     }
 
+    /**
+     * Load properties from a File and provides an option to restriction on overriding property.
+     *
+     * @param file File containing properties to load
+     * @param override Restriction on overriding property
+     * @throws Exception if override is false and duplicate property is found, an exception is raised.
+     */
     public static void loadProperties(File file, boolean override) throws Exception {
         loadProperties(new FileInputStream(file), override);
     }
 
+    /**
+     * Load properties from all files listed in propertiesFile.
+     *
+     * @param propertiesFile Contains map of file name containing properties to override option.
+     * @throws Exception if override is false and duplicate property is found, an exception is raised.
+     */
     public static void loadProperties(HashMap<String, Boolean> propertiesFile) throws Exception {
         for(Map.Entry<String, Boolean> entry : propertiesFile.entrySet()){
             loadProperties(new FileInputStream(entry.getKey()), entry.getValue());
         }
     }
 
+    /**
+     * Load properties from all files listed in propertiesFile and located in resources folder.
+     *
+     * @param propertiesFile Contains map of file name containing properties to override option.
+     * @throws Exception if override is false and duplicate property is found, an exception is raised.
+     */
     public static void loadPropertiesFromResources(HashMap<String, Boolean> propertiesFile)
             throws Exception {
         for(Map.Entry<String, Boolean> entry : propertiesFile.entrySet()){
@@ -43,6 +77,12 @@ public class ApplicationProperties {
         }
     }
 
+    /**
+     * Fetch a loaded property using key.
+     *
+     * @param key - Property name
+     * @return property associated with required property name.
+     */
     public static String getProperty(String key) {
         return properties.getProperty(key);
     }
